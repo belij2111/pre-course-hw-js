bc919fd2d4036cdea4d44533490b8243c91a8a5c
belij2111 <belijvalerij2111@gmail.com>
2023-12-16 14:05:47 +0300

    completed the task lesson-02/05

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+let newPassport = {};
+for (let key in passport) {
+    newPassport[key] = passport[key];
+}
+newPassport.name = "Ivan";
+console.log(passport.name);
+console.log(newPassport.name);
