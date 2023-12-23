94b48a5349571e4d548cc05c3ea3486b3531da6b
belij2111 <belijvalerij2111@gmail.com>
2023-12-23 15:27:16 +0300

    completed the task lesson-03/03

---
+++ lesson-03/03.js
@@ -0,0 +1,7 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+let sliceArray = function (array) {
+    console.log(array.slice(1, -2));
+}
+
+sliceArray(fruits);
